5fefc0266c62ea721100a9d09253adb0a602662b
uhammadziyo <DilafruzIminova71@gmail.com>
2024-11-09 10:29:55 +0500

    Add files via upload

---
+++ zadacha.py
@@ -0,0 +1,11 @@
+boy = 150
+ism = "Muhammadziyo"
+familiya = "Abduqodirov"
+adres = "chilonzor ц kavartal 20 31"
+play = "fc mobile"
+
+print(boy)
+print(ism)
+print(familiya)
+print(adres)
+print(play)
